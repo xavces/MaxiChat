04a9ca6542e881da6367a62ff64480391f582d29
xavces <maxime.bozouklian@ynov.com>
2019-05-03 12:37:36 +0200

    [03/05/2019] Ajout RecyclerView sur la page d'accueil

--- app/src/main/java/com/project/maxichat/MainActivity.kt
+++ app/src/main/java/com/project/maxichat/MainActivity.kt
@@ -2,12 +2,29 @@ package com.project.maxichat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerLayoutManager: RecyclerView.LayoutManager
+    private lateinit var recyclerAdapter: RecyclerView.Adapter<*>
+    private var dataset: ArrayList<String> = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        for (i in 1..20) {
+            dataset.add("Welcome to $i")
+        }
+        recyclerLayoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerAdapter = MainAdapter(dataset)
+        recyclerView.layoutManager = recyclerLayoutManager
+        recyclerView.adapter = recyclerAdapter
     }
 
 }
