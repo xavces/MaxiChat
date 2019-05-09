6a6b969152971be4c4327bceb35ecb7ff57b3980
Maximilien COSTA <maximilien.costa@ynov.com>
2019-05-09 11:06:02 +0200

    first commit

--- app/src/main/java/com/project/maxichat/LoginActivity.kt
+++ app/src/main/java/com/project/maxichat/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.project.maxichat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.singleTop
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonLogin.setOnClickListener{
+            startActivity(intentFor<MessageActivity>().singleTop())
+        }
     }
 }
